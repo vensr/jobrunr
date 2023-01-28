cb3d094d34666b817709775758c81311d00549c0
venkatesh <>
2023-01-28 18:28:17 +0530

    initial commit

---
+++ src/test/java/com/minilab/jobrunr/JobrunrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.minilab.jobrunr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobrunrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
